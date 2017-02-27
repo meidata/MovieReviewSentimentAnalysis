e2b2c7867e5914c38b882aa1e177ed569ea7153a
meidata <910330@gmail.com>
2017-02-27 10:03:02 +0100

    add more models

--- reviewsData.py
+++ reviewsData.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[10]:
+# In[2]:
 
 import re 
 import csv
@@ -31,136 +31,42 @@ def toc():
         print("Elapsed time is " + str(time.time() - startTime_for_tictoc) + " seconds..\n")
     else:
         print("Toc: start time not set...\n")
-        
-        
-with open('drama_love_reviews_100.json') as json_data:
+
+
+# In[ ]:
+
+with open('data/drama_love_reviews_100.json') as json_data:
     posReviewsDrama = json.load(json_data)
     
-with open('drama_hate_reviews_100.json') as json_data:
+with open('data/drama_love_reviews_100.json') as json_data:
+    posReviewsDrama = json.load(json_data)
+    
+with open('data/drama_hate_reviews_100.json') as json_data:
     negReviewsDrama = json.load(json_data)
     
-with open('action_love_reviews_100.json') as json_data:
+with open('data/action_love_reviews_100.json') as json_data:
     posReviewsAction = json.load(json_data)
     
-with open('action_hate_reviews_100.json') as json_data:
+with open('data/action_hate_reviews_100.json') as json_data:
     negReviewsAction  = json.load(json_data)
     
-with open('horror_hate_reviews_100.json') as json_data:
+with open('data/horror_hate_reviews_100.json') as json_data:
     negReviewsHorror  = json.load(json_data) 
     
-with open('horror_love_reviews_100.json') as json_data:
+with open('data/horror_love_reviews_100.json') as json_data:
     posReviewsHorror  = json.load(json_data) 
     
-with open('romance_hate_reviews_100.json') as json_data:
+with open('data/romance_hate_reviews_100.json') as json_data:
     negReviewsRomance  = json.load(json_data)     
     
-with open('romance_love_reviews_100.json') as json_data:
+with open('data/romance_love_reviews_100.json') as json_data:
     posReviewsRomance  = json.load(json_data) 
     
-with open('Sci-Fi_love_reviews_100.json') as json_data:
+with open('data/Sci-Fi_love_reviews_100.json') as json_data:
     posReviewsSci  = json.load(json_data) 
     
-with open('Sci-Fi_hate_reviews_100.json') as json_data:
+with open('data/Sci-Fi_hate_reviews_100.json') as json_data:
     negReviewsSci  = json.load(json_data) 
-
-
-#     
-# '''
-# posReList = []
-# for reviewlist in posReviewsDrama + posReviewsHorror + posReviewsAction + posReviewsSci + posReviewsRomance:
-#     if reviewlist is not None:
-#         for review in reviewlist:
-#             posReList.append([review['review'],review['categorie']])
-#             
-# posReviews = pd.DataFrame(posReList,columns=['reviews', 'sentiment'])
-# posReviews = posReviews.drop_duplicates()
-# 
-# negReList = []
-# for reviewlist in  negReviewsAction + negReviewsHorror +negReviewsDrama +negReviewsRomance + negReviewsSci:
-#     if reviewlist is not None:
-#         for review in reviewlist:
-#             negReList.append([review['review'],review['categorie']])
-#             
-# negReviews = pd.DataFrame(negReList,columns=['reviews', 'sentiment'])
-# negReviews = negReviews.drop_duplicates()
-# 
-# pos_train, pos_test = train_test_split(posReviews, test_size = 0.3)
-# neg_train, neg_test = train_test_split(negReviews, test_size = 0.3)
-# 
-# train = pos_train.append(neg_train, ignore_index=True)
-# test = pos_test.append(neg_test, ignore_index=True)
-# 
-# train = train.reset_index(drop=True)
-# train['id'] = train.index
-# 
-# 
-# test = test.reset_index(drop=True)
-# test['id'] = test.index
-# 
-# print('train data size..',train.shape,'..\n')
-# print('test data size..',test.shape,'..\n')
-# 
-# 
-# def textPrepocess(movieReviews):
-#     movieReviews = movieReviews.lower()
-#     movieReviews = re.sub("[^a-zA-Z]",' ',movieReviews)
-#     movieReviews = (" ".join(movieReviews.split()))
-#     return movieReviews
-# 
-# train['reviews'] = train['reviews'].apply(textPrepocess)
-# test['reviews'] = test['reviews'].apply(textPrepocess)
-# 
-# print('all data has been imported..\n')
-# 
-# '''
-
-# ### add titles
-
-# posTitlesList = []
-# for reviewlist in posReviewsDrama + posReviewsHorror + posReviewsAction + posReviewsSci + posReviewsRomance:
-#     if reviewlist is not None:
-#         for review in reviewlist:
-#             posTitlesList.append([review['title'],review['categorie']])
-#             
-# posTitles = pd.DataFrame(posTitlesList,columns=['reviews', 'sentiment'])
-# posTitles = posTitles.drop_duplicates()
-# 
-# negTitlesList = []
-# for reviewlist in  negReviewsAction + negReviewsHorror +negReviewsDrama +negReviewsRomance + negReviewsSci:
-#     if reviewlist is not None:
-#         for review in reviewlist:
-#             negTitlesList.append([review['title'],review['categorie']])
-#             
-#             
-# negTitles = pd.DataFrame(negTitlesList,columns=['reviews', 'sentiment'])
-# negTitles = negTitles.drop_duplicates()
-# pos_review_title = posTitles.append(posReviews, ignore_index=True)
-# neg_review_title = negTitles.append(negReviews, ignore_index=True)
-# 
-# pos_train_title, pos_test_title = train_test_split(pos_review_title, test_size = 0.3)
-# neg_train_title, neg_test_title = train_test_split(neg_review_title, test_size = 0.3)
-# 
-# train_title = pos_train_title.append(neg_train_title, ignore_index=True)
-# test_title = pos_test_title.append(neg_test_title, ignore_index=True)
-# 
-# train_title = train_title.reset_index(drop=True)
-# train_title['id'] = train_title.index
-# 
-# 
-# test_title = test_title.reset_index(drop=True)
-# test_title['id'] = test_title.index
-# 
-# print('train with titles data size..',train_title.shape,'..\n')
-# print('test with titles data size..',test_title.shape,'..\n')
-# 
-# 
-# train_title['reviews'] = train_title['reviews'].apply(textPrepocess)
-# test_title['reviews'] = test_title['reviews'].apply(textPrepocess)
-# 
-# print('all data with titles has been imported..\n')
-
-# In[11]:
-
 posReList = []
 for reviewlist in posReviewsDrama + posReviewsHorror + posReviewsAction + posReviewsSci + posReviewsRomance:
     if reviewlist is not None:
@@ -196,10 +102,10 @@ train['id'] = train.index
 test = test.reset_index(drop=True)
 test['id'] = test.index
 
-print('train data size..',train.shape,'..\n')
-print('test data size..',test.shape,'..\n')
+print('train data size:',train.shape,'..\n')
+print('test data size:',test.shape,'..\n')
 
-
+'''
 def textPrepocess(movieReviews):
     movieReviews = movieReviews.lower()
     movieReviews = re.sub("[^a-zA-Z]",' ',movieReviews)
@@ -210,4 +116,83 @@ train['reviews'] = train['reviews'].apply(textPrepocess)
 test['reviews'] = test['reviews'].apply(textPrepocess)
 
 print('all data has been imported..\n')
+'''
+
+
+# 
+# 
+# 
+# ### Unlabeled dataset for word2vec
+# 
+# 
+# 
+
+# In[6]:
+
+with open('data/Adventure_hate_reviews_100.json') as json_data:
+    negReviewsAdv  = json.load(json_data) 
+    
+with open('data/Adventure_love_reviews_100.json') as json_data:
+    posReviewsAdv  = json.load(json_data) 
+    
+with open('data/Fantasy_hate_reviews_100.json') as json_data:
+    negReviewsFan  = json.load(json_data) 
+    
+with open('data/Fantasy_love_reviews_100.json') as json_data:
+    posReviewsFan  = json.load(json_data) 
+    
+with open('data/Mystery_hate_reviews_100.json') as json_data:
+    negReviewsMys = json.load(json_data) 
+    
+with open('data/Mystery_love_reviews_100.json') as json_data:
+    posReviewsMys = json.load(json_data) 
+    
+with open('data/Comedy_hate_reviews_100.json') as json_data:
+    negReviewsCom = json.load(json_data) 
+    
+with open('data/Comedy_love_reviews_100.json') as json_data:
+    posReviewsCom = json.load(json_data) 
+    
+with open('data/Family_hate_reviews_100.json') as json_data:
+    negReviewsFam = json.load(json_data) 
+    
+with open('data/Family_love_reviews_100.json') as json_data:
+    posReviewsFam = json.load(json_data)     
+
+posRe = []
+for reviewlist in posReviewsAdv + posReviewsFan + posReviewsCom + posReviewsMys + posReviewsFam:
+    if reviewlist is not None:
+        for review in reviewlist:
+            posRe.append([review['review'],review['title']])
+                              #,review['categorie']])
+            
+pos_Reviews = pd.DataFrame(posRe,columns=['reviews','title'])
+                                             #,'sentiment'])
+pos_Reviews = pos_Reviews.drop_duplicates()
+pos_Reviews["reviews"] = pos_Reviews["reviews"].map(str) + pos_Reviews["title"]
+pos_Reviews = pos_Reviews.drop('title',1)
+
+negRe = []
+for reviewlist in  negReviewsAdv + negReviewsFan +negReviewsCom +negReviewsMys + negReviewsFam:
+    if reviewlist is not None:
+        for review in reviewlist:
+            negRe.append([review['review'],review['title']])
+                              #,review['categorie']])#
+            
+neg_Reviews = pd.DataFrame(negRe,columns=['reviews','title'])
+                                             #'sentiment'])
+neg_Reviews = neg_Reviews.drop_duplicates()
+neg_Reviews["reviews"] = neg_Reviews["reviews"].map(str) + neg_Reviews["title"]
+neg_Reviews = neg_Reviews.drop('title',1)
+
+
+unlabeled_reviews = pos_Reviews.append(neg_Reviews, ignore_index=True)
+
+
+unlabeled_reviews = unlabeled_reviews.reset_index(drop=True)
+unlabeled_reviews['id'] = unlabeled_reviews.index
+
+
+
+print('Unlabeled train data size :',unlabeled_reviews.shape,'..\n')
 
